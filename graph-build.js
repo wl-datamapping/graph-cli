b4167cda58439b2109248e592ab61a8fa5c266ac
Ford Nickels <ford@thegraph.com>
2018-08-06 14:18:33 -0700

    build, codegen: Exit watcher on SIGINT, longer wait for atomic changes

--- graph-build.js
+++ graph-build.js
@@ -4,9 +4,9 @@ let app = require('commander')
 let path = require('path')
 let ipfsAPI = require('ipfs-api')
 let chokidar = require('chokidar');
-let Compiler = require('./src/cli/compiler')
-
 const chalk = require('chalk')
+
+let Compiler = require('./src/cli/compiler')
 
 app
   .version('0.1.0')
@@ -23,7 +23,7 @@ app
   )
   .option('-t, --output-format [format]', 'Output format (wasm, wast)', 'wasm')
   .option('i, --ipfs [addr]', 'IPFS node to use for uploading files')
-  .option('-w, --watch', 'Setup directory watching to rebuild when files change')
+  .option('-w, --watch', 'Setup directory watching to rebuild when file changes are detected')
 
 app.on('--help', function() {
   console.log('')
@@ -49,49 +49,55 @@ if (file === null || file === undefined) {
 let ipfs = app.ipfs ? ipfsAPI(app.ipfs) : undefined
 
 let subgraph_generator = new Compiler({
-    ipfs: ipfs,
-    subgraphManifest: file,
-    outputDir: app.outputDir,
-    outputFormat: app.outputFormat,
-    verbosity: app.verbosity,
+  ipfs: ipfs,
+  subgraphManifest: file,
+  outputDir: app.outputDir,
+  outputFormat: app.outputFormat,
+  verbosity: app.verbosity,
 })
 
 // Watch working directory for file updates or additions, trigger compile (if watch argument specified)
 if (app.watch) {
-    subgraph_generator.logger.info('')
-    subgraph_generator.logger.info(chalk.grey("Watching files, a new build will be triggered when file updates or additions are detected"))
-    // Initialize watchers
-    let watcher = chokidar.watch('.', {
-        persistent: true,
-        ignoreInitial: true,
-        ignored: [
-            /(^|[\/\\])\../,
-            './node_modules',
-            './examples',
-            './dist',
-            '.yarn.lock'
-        ],
-        depth: 3,
-    })
+  subgraph_generator.logger.info('')
+  subgraph_generator.logger.info('%s %s', chalk.grey("Watching:"), process.cwd())
+  // Initialize watchers
+  let watcher = chokidar.watch('.', {
+    persistent: true,
+    ignoreInitial: true,
+    ignored: [
+      './dist',
+      './examples',
+      './node_modules',
+      /(^|[\/\\])\../
+    ],
+    depth: 3,
+    atomic: 500
+  })
 
-    // Add event listeners.
-    watcher
-        .on('ready', function() {
-            subgraph_generator.compile()
-            watcher
-                .on('add', path => {
-                    subgraph_generator.logger.info(chalk.grey('New file detected, rebuilding subgraph'))
-                    subgraph_generator.compile()
-                    subgraph_generator.logger.info('')
-                })
-                .on('change', path => {
-                    subgraph_generator.logger.info(chalk.grey('File change detected, rebuilding subgraph'))
-                    subgraph_generator.compile()
-                    subgraph_generator.logger.info('')
-            });
-            subgraph_generator.logger.info('')
-    })
+  // Add event listeners.
+  watcher
+    .on('ready', function() {
+      subgraph_generator.compile()
+      watcher
+        .on('add', path => {
+          subgraph_generator.logger.info(chalk.grey('New file detected, rebuilding subgraph'))
+          subgraph_generator.compile()
+          subgraph_generator.logger.info('')
+        })
+        .on('change', path => {
+          subgraph_generator.logger.info(chalk.grey('File change detected, rebuilding subgraph'))
+          subgraph_generator.compile()
+          subgraph_generator.logger.info('')
+      });
+      subgraph_generator.logger.info('')
+  })
+
+  //Catch keyboard interrupt: close watcher and exit process
+  process.on('SIGINT', function() {
+    watcher.close()
+    process.exit()
+  })
 } else {
-    subgraph_generator.compile()
+  subgraph_generator.compile()
 }
 
